40986e864da7522b08d5f25e977e6bd04e38ebd2
Francisco Zambrano <franciscozambrano@outlook.es>
2023-09-10 18:45:30 -0500

    feat: side nav implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuggestedComponent } from './components/suggested/suggested.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuggestedComponent,
+    SideNavComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/suggested/suggested.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suggested',
+  templateUrl: './suggested.component.html',
+  styleUrls: ['./suggested.component.scss']
+})
+export class SuggestedComponent {
+
+}
